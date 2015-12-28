964f754d7d91d2865666c8503f4d518011a6e6ac
mohd <mohd.muthanna@gmail.com>
2015-12-28 21:12:19 +0200

    Added local settings

--- blog/settings.py
+++ blog/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "rb0wyf5*s$hwvbbz))=hs+_+h_fkk@5@5ep+e44$#sm90ym#p*"
+SECRET_KEY = "rb0wyf5*s$hwvbbz))=hs+_+h_fkk@5@5ep+e44$#sm90ym##"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 
 # Application definition
